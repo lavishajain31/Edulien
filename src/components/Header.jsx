65cf80d885119cd6eea44704936fcf23da55e000
lavishajain31 <jainlavisha18@gmail.com>
2024-07-21 23:40:23 +0530

    start frontend

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { FaBook, FaStickyNote, FaList, FaUsers } from 'react-icons/fa';
 const Header = () => {
   return (
     <header className="bg-dark-bg text-white p-6 flex justify-between items-center shadow-md">
-      <h1 className="text-3xl font-bold">Welcome to Edulie!!</h1>
+      <h1 className="text-3xl font-bold">Welcome Edulien</h1>
       <div>
         <Link to="" className="bg-primary text-white px-4 py-2 rounded-lg hover:bg-primary-dark transition-colors mr-4">Contact Us</Link>
         <Link to="" className="bg-secondary text-white px-4 py-2 rounded-lg hover:bg-secondary-dark transition-colors">Profile</Link>
